b086d7b1dcd8cbabc576ced6b1b70fff562173ed
Will <willian.ralf.rn@gmail.com>
2023-04-01 22:17:35 -0300

    feat: firebase signup flow

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
@@ -17,16 +18,91 @@ import {
 } from 'native-base';
 import {StackScreenProps} from '@react-navigation/stack';
 import {MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
+import {Controller, FieldValues, useForm} from 'react-hook-form';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+  User as FirebaseUserType,
+} from 'firebase/auth';
 
 import {AuthRoutesParamsList} from 'src/shared/interfaces/routes';
 import {routes} from 'src/shared/constants/routes';
 import Button from 'src/components/Form/Button';
 import Input from 'src/components/Form/Input';
+import {app} from 'firebaseConfig';
+import {checkEmail} from 'src/shared/utils/checkEmail';
+import AuthContext from 'src/shared/contexts/AuthContext';
+
+interface FormProps extends FieldValues {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export default function SignUp({
   route,
   navigation,
 }: StackScreenProps<AuthRoutesParamsList, 'sign-up'>) {
+  const {saveUser} = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {control, handleSubmit} = useForm<FormProps>();
+
+  const auth = getAuth(app);
+
+  async function onSubmit(form: FormProps) {
+    if (!form.name || !form.email || !form.password || !form.confirmPassword) {
+      return Alert.alert(
+        'Meteor To Do',
+        'Por favor preencha os campos necessários para realizar o login!',
+      );
+    }
+
+    const isValid = checkEmail(form.email);
+    if (!isValid) {
+      return Alert.alert('Meteor To Do', 'Digite um e-mail válido!');
+    }
+
+    if (form.password !== form.confirmPassword) {
+      return Alert.alert('Meteor To Do', 'As duas senhas não coincidem!');
+    }
+
+    if (form.password.length < 6) {
+      return Alert.alert(
+        'Meteor To Do',
+        'Sua senha deve ter no mínimo 6 caracteres!',
+      );
+    }
+
+    setIsLoading(true);
+    await createUserWithEmailAndPassword(auth, form.email, form.password)
+      .then(() => {
+        updateProfile(auth.currentUser as FirebaseUserType, {
+          displayName: form.name,
+        })
+          .then(async () => {
+            Alert.alert('Meteor To Do', 'Usuário criado com sucesso!');
+            await saveUser(auth.currentUser as FirebaseUserType);
+            setIsLoading(false);
+          })
+          .catch(error => {
+            setIsLoading(false);
+            Alert.alert('Meteor To Do', error.message);
+          });
+      })
+      .catch(error => {
+        setIsLoading(false);
+
+        if (error.code === 'auth/email-already-in-use') {
+          return Alert.alert('Meteor To Do', 'E-mail já cadastrado!');
+        }
+        Alert.alert('Meteor To Do', error.message);
+      });
+  }
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <KeyboardAvoidingView
@@ -68,73 +144,110 @@ export default function SignUp({
             </Text>
             <Box mt={6}>
               <Text color="gray.700">Nome</Text>
-              <Input
-                placeholder="Digite o seu nome"
-                autoCapitalize="none"
-                leftElement={
-                  <Box ml={2}>
-                    <MaterialIcons
-                      name="person-outline"
-                      size={24}
-                      color={theme.colors.gray[700]}
-                    />
-                  </Box>
-                }
+              <Controller
+                name="name"
+                control={control}
+                render={({field: {onChange, value}}) => (
+                  <Input
+                    placeholder="Digite o seu nome"
+                    autoCapitalize="none"
+                    leftElement={
+                      <Box ml={2}>
+                        <MaterialIcons
+                          name="person-outline"
+                          size={24}
+                          color={theme.colors.gray[700]}
+                        />
+                      </Box>
+                    }
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                )}
               />
             </Box>
             <Box mt={2}>
               <Text color="gray.700">E-mail</Text>
-              <Input
-                placeholder="Digite o seu e-mail"
-                autoCapitalize="none"
-                leftElement={
-                  <Box ml={2}>
-                    <MaterialCommunityIcons
-                      name="email-outline"
-                      size={24}
-                      color={theme.colors.gray[700]}
-                    />
-                  </Box>
-                }
+              <Controller
+                name="email"
+                control={control}
+                render={({field: {onChange, value}}) => (
+                  <Input
+                    placeholder="Digite o seu e-mail"
+                    autoCapitalize="none"
+                    leftElement={
+                      <Box ml={2}>
+                        <MaterialCommunityIcons
+                          name="email-outline"
+                          size={24}
+                          color={theme.colors.gray[700]}
+                        />
+                      </Box>
+                    }
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                )}
               />
             </Box>
             <Box mt={2}>
               <Text color="gray.700">Senha</Text>
-              <Input
-                isPassword
-                placeholder="Digite a sua senha"
-                autoCapitalize="none"
-                leftElement={
-                  <Box ml={2}>
-                    <MaterialCommunityIcons
-                      name="lock-outline"
-                      size={24}
-                      color={theme.colors.gray[700]}
-                    />
-                  </Box>
-                }
+              <Controller
+                name="password"
+                control={control}
+                render={({field: {onChange, value}}) => (
+                  <Input
+                    isPassword
+                    placeholder="Digite a sua senha"
+                    autoCapitalize="none"
+                    leftElement={
+                      <Box ml={2}>
+                        <MaterialCommunityIcons
+                          name="lock-outline"
+                          size={24}
+                          color={theme.colors.gray[700]}
+                        />
+                      </Box>
+                    }
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                )}
               />
             </Box>
             <Box mt={2}>
               <Text color="gray.700">Confirmar denha</Text>
-              <Input
-                isPassword
-                placeholder="Confirme sua senha"
-                autoCapitalize="none"
-                leftElement={
-                  <Box ml={2}>
-                    <MaterialCommunityIcons
-                      name="lock-outline"
-                      size={24}
-                      color={theme.colors.gray[700]}
-                    />
-                  </Box>
-                }
+              <Controller
+                name="confirmPassword"
+                control={control}
+                render={({field: {onChange, value}}) => (
+                  <Input
+                    isPassword
+                    placeholder="Confirme sua senha"
+                    autoCapitalize="none"
+                    leftElement={
+                      <Box ml={2}>
+                        <MaterialCommunityIcons
+                          name="lock-outline"
+                          size={24}
+                          color={theme.colors.gray[700]}
+                        />
+                      </Box>
+                    }
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                )}
               />
             </Box>
           </VStack>
           <VStack mb={2} p={4} backgroundColor="gray.50">
-            <Button title="Cadastrar" type="primary" />
+            <Button
+              isLoading={isLoading}
+              title="Cadastrar"
+              type="primary"
+              onPress={handleSubmit(onSubmit)}
+            />
             <TouchableOpacity
               style={{alignSelf: 'center'}}
               onPress={() => navigation.navigate(routes.auth.login as never)}>
