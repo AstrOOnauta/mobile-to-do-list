4fca1ae6b1129d0becc5684b44f0e058bafadb01
Will <willian.ralf.rn@gmail.com>
2023-04-03 07:57:24 -0300

    feat: integration with realtime db - graphic tab screen

--- src/pages/Graphics/index.tsx
+++ src/pages/Graphics/index.tsx
@@ -1,41 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {Box, FlatList, HStack, Icon, Pressable, Text, theme} from 'native-base';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  Box,
+  FlatList,
+  HStack,
+  Icon,
+  Pressable,
+  Skeleton,
+  Text,
+  theme,
+} from 'native-base';
 import {StackScreenProps} from '@react-navigation/stack';
+import {useIsFocused} from '@react-navigation/native';
 import {VictoryLegend, VictoryPie} from 'victory-native';
 import {MaterialIcons} from '@expo/vector-icons';
 import {addMonths, subMonths, format, isSameMonth} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import {child, get, getDatabase, ref} from 'firebase/database';
 
 import {HomeRoutesParamsList} from 'src/shared/interfaces/routes';
 import {Task} from 'src/shared/interfaces/models/Task';
 import TaskCard from 'src/components/TaskCard';
-
-const TASKS_DATA = [
-  {
-    id: 0,
-    title: 'Task1',
-    description: 'Lorem Ipsum...',
-    isDone: false,
-    doneAt: null,
-    createdAt: new Date('2023-04-02T12:57:36.458Z'),
-  },
-  {
-    id: 1,
-    title: 'Task2',
-    description: 'Lorem Ipsum... Lorem Ipsum... Lorem afah aogkoasg aggh',
-    isDone: true,
-    doneAt: new Date('2023-04-02T12:57:36.458Z'),
-    createdAt: new Date('2023-04-02T12:57:36.458Z'),
-  },
-  {
-    id: 3,
-    title: 'Task3',
-    description: 'Lorem Ipsum... Lorem Ipsum... Lorem afah aogkoasg aggh',
-    isDone: true,
-    doneAt: new Date('2023-04-02T12:57:36.458Z'),
-    createdAt: new Date('2023-04-02T12:57:36.458Z'),
-  },
-];
+import AuthContext from 'src/shared/contexts/AuthContext';
 
 interface GraphicDataProps {
   x: string;
@@ -46,9 +31,38 @@ export default function Graphics({
   route,
   navigation,
 }: StackScreenProps<HomeRoutesParamsList, 'graphics'>) {
+  const {user} = useContext(AuthContext);
+
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [graphicData, setGraphicData] = useState<GraphicDataProps[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [fitleredTasks, setFilteredTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const isFocused = useIsFocused();
+  const db = getDatabase();
+
+  async function getTasks() {
+    setIsLoading(true);
+    const tasksDB = ref(db);
+
+    await get(child(tasksDB, `tasks/user${user.uid}`))
+      .then(async snapshot => {
+        setTasks([]);
+        const data = await snapshot.val();
+
+        const dataKeys = Object.keys(data);
+
+        for (let i = 0; i < dataKeys.length; i++) {
+          setTasks(oldTasks => [...oldTasks, data[dataKeys[i]]] as Task[]);
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+
+    setIsLoading(false);
+  }
 
   function handleDateChange(action: 'prev' | 'next') {
     if (action === 'prev') {
@@ -59,8 +73,8 @@ export default function Graphics({
   }
 
   function transformDataToGraphic() {
-    const selectedDateTasks = TASKS_DATA.filter(item => {
-      return isSameMonth(item.createdAt, selectedDate);
+    const selectedDateTasks = tasks.filter(item => {
+      return isSameMonth(new Date(item.createdAt!), selectedDate);
     });
 
     const doneTasks = selectedDateTasks.filter(item => {
@@ -77,7 +91,7 @@ export default function Graphics({
 
     let data: GraphicDataProps[] = [];
 
-    if (done > 0 && undone > 0) {
+    if (done > 0 || undone > 0) {
       data = [
         {x: done === 0 ? ' ' : `${done.toFixed(1)}%`, y: done},
         {x: undone === 0 ? ' ' : `${undone.toFixed(1)}%`, y: undone},
@@ -89,8 +103,16 @@ export default function Graphics({
   }
 
   useEffect(() => {
-    transformDataToGraphic();
-  }, [selectedDate, TASKS_DATA]);
+    if (isFocused) {
+      getTasks();
+    }
+  }, [isFocused]);
+
+  useEffect(() => {
+    if (tasks.length) {
+      transformDataToGraphic();
+    }
+  }, [selectedDate, tasks]);
 
   return (
     <Box flex={1} backgroundColor="gray.50" pt={8} px={4}>
@@ -98,7 +120,8 @@ export default function Graphics({
         w="70%"
         justifyContent="space-between"
         alignItems="center"
-        alignSelf="center">
+        alignSelf="center"
+        zIndex={2}>
         <Pressable
           backgroundColor="amber.300"
           p={1}
@@ -129,45 +152,78 @@ export default function Graphics({
           />
         </Pressable>
       </HStack>
-      <Box alignSelf="center" mb={-20}>
-        {graphicData.length ? (
-          <HStack maxW="100%">
-            <VictoryPie
-              data={graphicData}
-              colorScale={[theme.colors.amber[300], theme.colors.gray[700]]}
-              labelRadius={72}
-              width={230}
-              height={230}
-              style={{
-                labels: {
-                  fontSize: '14px',
-                  fontWeight: 'bold',
-                  fill: theme.colors.gray[700],
-                },
-              }}
-              animate={{
-                duration: 1000,
-              }}
+      {isLoading ? (
+        <Box flex={1}>
+          <HStack mt={12} ml={1} alignItems="flex-end">
+            <Skeleton
+              borderWidth={1}
+              borderColor="coolGray.200"
+              startColor="gray.200"
+              endColor="gray.400"
+              size={32}
+              rounded="full"
+              ml={12}
+              mt={3}
             />
-            <VictoryLegend
-              x={12}
-              y={130}
-              colorScale={[theme.colors.amber[300], theme.colors.gray[700]]}
-              data={[{name: 'Feitas'}, {name: 'Não feitas'}]}
+            <Skeleton.Text
+              lines={2}
+              ml={12}
+              w={24}
+              startColor="gray.200"
+              endColor="gray.400"
             />
           </HStack>
-        ) : (
-          <Box
-            width={250}
-            height={250}
-            alignItems="center"
-            justifyContent="center">
-            <Text>Nenhuma tarefa registrada...</Text>
+          <Box mt={12}>
+            <Skeleton.Text
+              lines={1}
+              w={32}
+              startColor="gray.200"
+              endColor="gray.400"
+              mb={4}
+            />
+            {[...Array(3).keys()].map(index => {
+              return (
+                <Skeleton
+                  key={index}
+                  my={2}
+                  h={16}
+                  rounded="md"
+                  startColor="gray.200"
+                  endColor="gray.400"
+                />
+              );
+            })}
           </Box>
-        )}
-      </Box>
-      {fitleredTasks.length ? (
+        </Box>
+      ) : fitleredTasks.length ? (
         <>
+          <Box alignSelf="center" mb={-20}>
+            <HStack maxW="100%">
+              <VictoryPie
+                data={graphicData}
+                colorScale={[theme.colors.amber[300], theme.colors.gray[700]]}
+                labelRadius={72}
+                width={230}
+                height={230}
+                style={{
+                  labels: {
+                    fontSize: '14px',
+                    fontWeight: 'bold',
+                    fill: theme.colors.gray[700],
+                  },
+                }}
+                animate={{
+                  duration: 1000,
+                }}
+              />
+              <VictoryLegend
+                x={12}
+                y={130}
+                colorScale={[theme.colors.amber[300], theme.colors.gray[700]]}
+                data={[{name: 'Feitas'}, {name: 'Não feitas'}]}
+              />
+            </HStack>
+          </Box>
           <Text fontSize="lg" fontWeight="bold" color="violet.600" mb={2}>
             Taferas ({fitleredTasks.length})
           </Text>
@@ -175,11 +231,17 @@ export default function Graphics({
             showsVerticalScrollIndicator={false}
             data={fitleredTasks}
             renderItem={({item, index}) => {
-              return <TaskCard key={index} item={item} />;
+              return <TaskCard key={index} item={item} getTasks={getTasks} />;
             }}
           />
         </>
-      ) : null}
+      ) : (
+        <Box flex={1} alignItems="center" justifyContent="center" mt={-20}>
+          <Text color="gray.500" fontSize="lg" fontWeight="medium">
+            Nenhuma tarefa registrada...
+          </Text>
+        </Box>
+      )}
     </Box>
   );
 }
